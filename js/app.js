afc1d808e5d1468ac4d2eab9b9bccfd463539af7
MadrimovS <sardormadrimov516@gmail.com>
2022-12-21 18:25:31 +0500

    main

--- js/app.js
+++ js/app.js
@@ -5,37 +5,33 @@ const walking = document.querySelector(".walk"),
   elInput = document.querySelector(".input"),
   elForm = document.querySelector(".form");
 
+function generateTime(space, speed) {
+  const hour = Math.floor(space / speed),
+    minute = Math.floor((space / speed - hour) * 60),
+    sec = Math.floor(((space / speed - hour) * 60 - minute) * 60);
+
+  return `${hour} soat ${minute} minut ${sec} sekund`;
+}
+
 elForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
-  const person = Number(elInput.value);
+  const space = Number(elInput.value);
   elInput.value = "";
-  if (!isNaN(person)) {
-    const walk = 3.6,
-      bice = 20.1,
-      car = 70,
-      plane = 800;
-    //walk
-    const walkHour = Math.floor(person / walk),
-      walkMinute = Math.floor((person / walk - walkHour) * 60),
-      walkSec = Math.floor(((person / walk - walkHour) * 60 - walkMinute) * 60);
-    //bice
-    const biceHour = Math.floor(person / bice),
-      biceMinute = Math.floor((person / bice - biceHour) * 60),
-      biceSec = Math.floor(((person / bice - biceHour) * 60 - biceMinute) * 60);
-    //car
-    const carHour = Math.floor(person / car),
-      carMinute = Math.floor((person / car - carHour) * 60),
-      carSec = Math.floor(((person / car - carHour) * 60 - carMinute) * 60);
-    //plane
-    const planeHour = Math.floor(person / plane),
-      planeMinute = Math.floor((person / plane - planeHour) * 60),
-      planeSec = Math.floor(
-        ((person / plane - planeHour) * 60 - planeMinute) * 60
-      );
-    //result
-    walking.textContent = `${walkHour} soat ${walkMinute} minut ${walkSec} sekund`;
-    byBice.textContent = `${biceHour} soat ${biceMinute} minut ${biceSec} sekund`;
-    byCar.textContent = `${carHour} soat ${carMinute} minut ${carSec} sekund`;
-    byPlane.textContent = `${planeHour} soat ${planeMinute} minut ${planeSec} sekund`;
+
+  const walk = 3.6,
+    bice = 20.1,
+    car = 70,
+    plane = 800;
+
+  if (!isNaN(space)) {
+    const personTime = generateTime(space, walk),
+      biceTime = generateTime(space, bice),
+      carTime = generateTime(space, car),
+      planeTime = generateTime(space, plane);
+
+    walking.textContent = personTime;
+    byBice.textContent = biceTime;
+    byCar.textContent = carTime;
+    byPlane.textContent = planeTime;
   }
 });
